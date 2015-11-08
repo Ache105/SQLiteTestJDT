0d4d81b721f47048a7f7daadfd2c5319e1c088a2
takahararyuji <fp7185@gmail.com>
2015-11-08 21:33:04 +0900

    InitialCommit

---
+++ SQLitetest/src/jp/ne/tin/sqlite/Sqlite.java
@@ -0,0 +1,5 @@
+package jp.ne.tin.sqlite;
+
+public class Sqlite {
+
+}
---
+++ SQLitetest/src/jp/ne/tin/sqlitetest/EntryPoint.java
@@ -0,0 +1,54 @@
+package jp.ne.tin.sqlitetest;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class EntryPoint extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EntryPoint frame = new EntryPoint();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public EntryPoint() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JButton btnSaitama = new JButton("saitama");
+		btnSaitama.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Click");
+			}
+		});
+		contentPane.add(btnSaitama, BorderLayout.CENTER);
+	}
+
+}
